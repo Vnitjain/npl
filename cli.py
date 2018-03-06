bee6042a02edaeb340cc13806417369ede2b14ff
Vnitjain <jain.vinit14@gmail.com>
2018-03-06 20:10:38 +0530

    Add files via upload
    tp

--- cli.py
+++ cli.py
@@ -1,7 +1,12 @@
 import socket
+#setup
 ip_add,port = "127.0.0.1",4000
 ser_sd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ser_sd.connect((ip_add,port))
+#
 msg = ser_sd.recv(1024)
 print(msg.decode('ascii'))
+msg = input()
+ser_sd.send(msg.encode('ascii'))
+#
 ser_sd.close()
--- ser.py
+++ ser.py
@@ -1,9 +1,24 @@
-import socket
+import socket,pymysql
+#setup
 ip_add,port = "127.0.0.1",4000
 ser_sd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ser_sd.bind((ip_add,port))
 ser_sd.listen(5)
 cli_sd,cli_add = ser_sd.accept()
+#setup pymysql
+db = pymysql.connect("localhost","root","123456","test")
+cursor = db.cursor()
+#
 msg = "Enter Name\n> "
 cli_sd.send(msg.encode('ascii'))
+msg = cli_sd.recv(1024)
+user_name = msg.decode('ascii')
+
+sql = "select * from user where uname=\""+str(user_name)+"\";"
+cursor.execute(sql)
+rows = cursor.fetchall()
+for i in rows:
+	for j in range(3):
+		print(i[j])
+#
 cli_sd.close()
