b345a406da5395c7068de94db3089f7b6e53e1e5
Vinit Jain <jain.vinit14@gmail.com>
2018-09-09 14:19:36 +0530

    Add files via upload

--- cli.py
+++ cli.py
@@ -1,12 +1,215 @@
-import socket
+import socket,os,time
 #setup
-ip_add,port = "127.0.0.1",4000
+ip_add,port = "127.0.0.1",4001
 ser_sd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ser_sd.connect((ip_add,port))
 #
+
+#global variables
+defaultpath = "/home/vnit/users/"
+#---------------------------------------------------------------------------------------
+
+def animmenu(menu):
+	pline = ""
+	for i in range(len(menu)):
+		for j in range(len(menu[i])):
+			pline = pline + menu[i][j]
+			print(pline)
+			time.sleep(0.01)
+			os.system("clear")
+	print(pline,end="")
+	#time.sleep(3)
+#---------------------------------------------------------------------------------------
+
+def maininterface(email):
+	name,path = ser_sd.recv(1024).decode('ascii').split(',')
+	print(name+" "+path)
+	curdir = ""
+	userdir = name+"/"
+	while(True):
+		#getting directory and username
+		menu2 = ['\t\t\tWelcome to main interface','\n---------------------','\n1. Upload File','\n2. Download File','\n3. List Files','\n4. Change directory','\n5. Logout','\n---------------------','\nCurrent directory : ']
+		animmenu(menu2)
+		print(defaultpath+userdir+curdir)
+		op = int(input("Select\n> "))
+
+		#---------------------------------------------------------------------------------------
+
+		if(op==1):#upload
+			ser_sd.send("upload".encode('ascii'))
+			#send path
+			ser_sd.send((defaultpath+userdir+curdir).encode('ascii'))
+
+			#sending filename
+			filename = str(input("\nEnter name of the file\n> "))
+			ser_sd.send(filename.encode('ascii'))
+			if(os.path.isfile("/home/vnit/users/"+filename)):
+				ser_sd.send("exists".encode('ascii'))
+				if(".txt" in filename):
+					fp = open("/home/vnit/users/"+filename,"r")
+					format = "txt"
+					contents = fp.read()
+					#sending file size and format
+					ser_sd.send((str(len(contents))+","+str(format)).encode('ascii'))
+					ser_sd.recv(1024).decode('ascii')#has no use
+					print("filename sent")
+					ser_sd.send(contents.encode('ascii'))
+				if(".jpg" in filename):
+					fp = open("/home/vnit/users/"+filename,"rb")
+					format = "jpg"
+					contents = fp.read()
+					#sending file size and format
+					ser_sd.send((str(len(contents))+","+str(format)).encode('ascii'))
+					ser_sd.recv(1024).decode('ascii')#has no use
+					print("filename sent")
+					ser_sd.send(contents)
+			else:
+				ser_sd.send("nexists".encode('ascii'))
+				print("File doesn't exist")
+			time.sleep(3)
+
+
+
+		#---------------------------------------------------------------------------------------
+
+		elif(op==2):#download
+			ser_sd.send("download".encode('ascii'))
+
+			#sending path
+			ser_sd.send((defaultpath+userdir+curdir).encode('ascii'))
+
+			#send a filename
+			filename = input("Enter a file to download\n> ")
+			ser_sd.send(filename.encode('ascii'))
+			#file exists or not
+			if(ser_sd.recv(1024).decode('ascii')=="exists"):
+				#receiving size of the file and format
+				filesize,format = ser_sd.recv(1024).decode('ascii').split(',')
+				filesize = int(filesize) + 1
+				#receiving file according to size
+				if(format=="txt"):
+					contents = ser_sd.recv(filesize).decode('ascii')
+					fp = open("/home/vnit/users/"+filename,"w+")
+				elif(format=="jpg"):
+					contents = ser_sd.recv(filesize)
+					fp = open("/home/vnit/users/"+filename,"wb")
+				fp.write(contents)
+				fp.close()
+			else:
+				print("File does not exist")
+				time.sleep(3)
+
+		#---------------------------------------------------------------------------------------
+		elif(op==3):#list files
+			ser_sd.send("list".encode('ascii'))
+
+			#sending the path
+			ser_sd.send((defaultpath+userdir+curdir).encode('ascii'))
+			print("List of files and folders")
+			#getting list
+			dir1 = list(ser_sd.recv(1024).decode('ascii').split(','))
+			if(dir1[0]=="blank"):
+				print("blank")
+			else:
+				for i in dir1:
+					print(i)
+			input("\n>")
+		#---------------------------------------------------------------------------------------
+		elif(op==4):#change directory
+			ser_sd.send("changedir".encode('ascii'))
+
+			curdir = input("Enter path\n> "+userdir)+"/"
+			#sending the path
+			ser_sd.send((defaultpath+userdir+curdir).encode('ascii'))
+			if("../" in curdir):
+				curdir = curdir[:len(curdir)-3]
+			#file exists or not
+			if(ser_sd.recv(1024).decode('ascii')=="exists"):
+				pass
+			else:
+				curdir = ""
+				print("Directory does not exist")
+				time.sleep(2)
+		#---------------------------------------------------------------------------------------
+		elif(op==5):#logout
+			ser_sd.send("logout".encode('ascii'))
+			return
+
+#---------------------------------------------------------------------------------------
+
+menu1 = ['\t\t\tCentral Repository','\nMenu','\n---------------------','\n1. Login','\n2. Register','\n3. Exit','\n---------------------\n'] #menu
+
+while(True):
+	# for i in menu1:
+	# 	print(i)
+	animmenu(menu1)
+	option = input("\n> ")
+#---------------------------------------------------------------------------------------
+	if(option=="1" or option=="Login" or option=="login"):
+		print("Login selected")
+		text1 = "login"
+		ser_sd.send(text1.encode('ascii'))
+
+		var1 = False
+		while(var1 == False):
+			email = input("Enter E-mail Id\n> ")
+			ser_sd.send(email.encode('ascii'))
+			reply = ser_sd.recv(1024)
+			if(str(reply.decode('ascii'))=="error"):
+					os.system("clear")
+					print("Email does not exist/Invalid email/nTry again")
+			else:
+					var1 = True
+					print("Approved")
+		#email has been approved
+		ser_sd.send(input("Enter password\n> ").encode('ascii'))
+		if(ser_sd.recv(1024).decode('ascii')=="access"):
+			print("Approved\n")
+			maininterface(email)
+		else:
+			print("Wrong password\nTry again")
+
+#---------------------------------------------------------------------------------------
+	elif(option=="2" or option=="Register" or option=="register"):
+		print("Register selected")
+		text1 = "register"
+		ser_sd.send(text1.encode('ascii'))
+		var1 = False
+		while(var1!=True):
+			#send email
+			ser_sd.send(input("Enter email\n> ").encode('ascii'))
+			if(ser_sd.recv(1024).decode('ascii')=="access"):
+				#send password
+				ser_sd.send(input("Enter password\n> ").encode('ascii'))
+
+				#send name
+				ser_sd.send(input("Enter a username\n> ").encode('ascii'))
+				var1 = True
+			else:
+				print("Try Again\nInvalid Email\Already exists\n")
+		#all values sent
+		if(ser_sd.recv(1024).decode('ascii')):
+			print("All values sent")
+		else:
+			print("There was an error\nTry again\n")
+#---------------------------------------------------------------------------------------
+	elif(option=="3" or option=="Exit" or option=="exit"):
+		print("exit")
+		text1 = "exit"
+		ser_sd.send(text1.encode('ascii'))
+		exit(0)
+
+	else:
+		print("Andha hai kya?")
+#---------------------------------------------------------------------------------------
+
+#
+'''
 msg = ser_sd.recv(1024)
-print(msg.decode('ascii'))
+print(msg.decode('ascii'),end="")
 msg = input()
 ser_sd.send(msg.encode('ascii'))
+print("")
+'''
 #
 ser_sd.close()
--- ser.py
+++ ser.py
@@ -1,14 +1,247 @@
-import socket,pymysql
+import socket,time,_thread,os
+from db_conf import *
+cursor = db_config()
 #setup
-ip_add,port = "127.0.0.1",4000
+ip_add,port = "",4002
 ser_sd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ser_sd.bind((ip_add,port))
 ser_sd.listen(5)
-cli_sd,cli_add = ser_sd.accept()
-#setup pymysql
-db = pymysql.connect("localhost","root","123456","test")
-cursor = db.cursor()
+
+#cli_sd,cli_add = ser_sd.accept()#this goes in the thread
+
+
 #
+#global variables
+defaultpath = "/home/vnit/users/"
+noofthreads = 2
+
+#---------------------------------------------------------------------------------------
+def getnamepath(email):
+	sql = "select name,path from user where email=\""+email+"\""
+	cursor.execute(sql)
+	rows = cursor.fetchall()
+	#rows[0][0] = name
+	#rows[0][1] = path
+	return rows[0][0]+","+rows[0][1]
+
+def addtodatabase(email,passw,name):
+	path = name+"/";
+	sql = "insert into user(email,password,name,path,id) values(\""+email+"\",\""+passw+"\",\""+name+"\",\""+path+"\",0)"
+	if(cursor.execute(sql)):
+		cursor.execute("commit")
+		return True
+	else:
+		return False
+
+def passwordchecker(email,passw):
+	sql = "select password from user where email = \""+email+"\";"
+	cursor.execute(sql)
+	rows = cursor.fetchall()
+	if(rows[0][0]==passw):
+		return True
+	else:
+		return False
+
+
+def validemailid(email,mode):
+	if(("@" in email) and ("." in email)):
+		if(mode=="r"):
+			mode = 0
+		elif(mode=="l"):
+			mode = 1
+		sql = "select email from user where email =\""+email+"\";";
+		if(cursor.execute(sql)==mode):
+			return True
+		else:
+			return False
+	else:
+		return False
+
+#---------------------------------------------------------------------------------------
+def func1(id):
+	while(True):
+		cli_sd,cli_add = ser_sd.accept()
+		print("\nid : "+str(id))
+		while(True):
+			print("menu1 reached")
+			text1 = cli_sd.recv(1024)
+			text1 = text1.decode('ascii')
+			#---------------------------------------------------------------------------------------
+			if(text1=="login"):
+				print("in login")
+				access = 0
+				while(access!=1):#email verification
+					try:
+						temp1 = cli_sd.recv(1024)#email
+						temp1 = temp1.decode('ascii')
+
+						if(validemailid(temp1,"l")):
+							access = 1
+							email_cli = temp1
+							cli_sd.send(str("access").encode('ascii'))
+						else:
+							cli_sd.send(str("error").encode('ascii'))
+					except:
+						break
+
+				if(passwordchecker(email_cli,cli_sd.recv(1024).decode('ascii'))):
+					cli_sd.send("access".encode('ascii'))
+					#password is verified
+					maininterface(email_cli,cli_sd)
+				else:
+					cli_sd.send("error".encode('ascii'))
+
+
+
+			#------------------------------------------------------------------------------------
+			elif(text1=="register"):
+				print("in register")
+				var1 = False
+				while(var1!=True):
+					email_cli = cli_sd.recv(1024)
+					email_cli = email_cli.decode('ascii')
+					if(validemailid(email_cli,"r")):
+						cli_sd.send("access".encode('ascii'))
+						print("access sent")
+						var1 = True
+					else:
+						print("error sent")
+						cli_sd.send("error".encode('ascii'))#email is verified
+
+				#recieve a password
+				print("getting password")
+				pass_cli = cli_sd.recv(1024).decode('ascii')
+				print(pass_cli)
+
+				#recieve a name
+				print("getting name")
+				name_cli = cli_sd.recv(1024).decode('ascii')
+
+				#add all values to database
+				if(addtodatabase(email_cli,pass_cli,name_cli)):
+					cli_sd.send("access".encode('ascii'))
+				else:
+					cli_sd.send("error".encode('ascii'))
+
+				#create folder
+				os.mkdir("/home/vnit/users/"+name_cli)
+
+			#---------------------------------------------------------------------------------------
+			elif(text1=="exit"):
+				#session[]
+				print("id : "+str(id)+" closed\n")
+				cli_sd.close()
+				break
+			#---------------------------------------------------------------------------------------
+
+def maininterface(email,cli_sd):
+	print("maininterface reached")
+	cli_sd.send(getnamepath(email).encode('ascii'))#sending name and path
+	while(True):
+		print("maininterface menu selection")
+		option = cli_sd.recv(1024).decode('ascii')
+		if(option=="upload"):#upload
+			print("in path")
+			#receiving path
+			path = cli_sd.recv(1024).decode('ascii')
+			print(path)
+			#receving filename
+			filename = cli_sd.recv(1024).decode('ascii')
+			filename = filename
+			#file exists or not
+			if(cli_sd.recv(6).decode('ascii')=="exists"):
+				print("receving file")
+				#receiving file size and format
+				size,format = cli_sd.recv(1024).decode('ascii').split(",")
+				cli_sd.send("abc".encode('ascii'))#temporary, has no use
+				size = int(size) + 1
+				contents = cli_sd.recv(size)
+				#print(str(size)+" "+format+" "+contents.decode('ascii'))
+				#receive file according to size
+				if(format=="txt"):
+					print("in txt")
+					fp = open(path+filename,"w+")
+					print(str(size)+" "+format+" "+contents.decode('ascii'))
+					contents = contents.decode('ascii')
+				elif(format=="jpg"):
+					print(str(size)+" "+format+" "+str(contents))
+					fp = open(path+filename,"wb")
+				fp.write(contents)
+				fp.close()
+			else:
+				print("in else")
+				pass
+
+		elif(option=="download"):#download
+			print("in download")
+			#receiving path
+			path = cli_sd.recv(1024).decode('ascii')
+			print("received path : "+path)
+			#receiving filename
+			filename = cli_sd.recv(1024).decode('ascii')
+			print("received filename : "+filename)
+
+			if(os.path.isfile(path+filename)):
+				cli_sd.send("exists".encode('ascii'))
+				if(".txt" in filename):
+					fp = open(path+filename,"r")
+					format = "txt"
+					contents = fp.read()
+					#sending file size
+					cli_sd.send((str(len(contents))+","+str(format)).encode('ascii'))
+					cli_sd.send(contents.encode('ascii'))
+				elif(".jpg" in filename):
+					fp = open(path+filename,"rb")
+					format = "jpg"
+					contents = fp.read()
+					#sending file size
+					cli_sd.send((str(len(contents))+","+str(format)).encode('ascii'))
+					cli_sd.send(contents)
+				print("reached end")
+			else:
+				cli_sd.send("nexists".encode('ascii'))
+
+		elif(option=="list"):#list files
+			path = cli_sd.recv(1024).decode('ascii')
+			dirlist = ""
+			for i in os.listdir(path):
+				dirlist = dirlist + i +","
+			dirlist = dirlist[0:len(dirlist)-1]
+			print(dirlist)
+			if(len(dirlist)==0):
+				dirlist = "blank"
+			cli_sd.send(dirlist.encode('ascii'))#sending dir list
+
+		elif(option=="changedir"):#change directory
+			print("in changedir")
+			path = cli_sd.recv(1024).decode('ascii')
+			if(os.path.isdir(path)):
+				cli_sd.send("exists".encode('ascii'))
+			else:
+				cli_sd.send("nexists".encode('ascii'))
+
+		elif(option=="logout"):#logout
+			return
+
+#---------------------------------------------------------------------------------------
+
+
+try:
+	for i in range(noofthreads):
+		_thread.start_new_thread(func1,(int(i)+1,))
+	# _thread.start_new_thread(func1,(1,))
+	# _thread.start_new_thread(func1,(2,))
+except:
+	pass
+'''
+passw = cli_sd.recv(1024)
+passw = passw.decode('ascii')
+'''
+'''
+while(True):
+	pass
+'''
+'''
 msg = "Enter Name\n> "
 cli_sd.send(msg.encode('ascii'))
 msg = cli_sd.recv(1024)
@@ -18,7 +251,11 @@ sql = "select * from user where uname=\""+str(user_name)+"\";"
 cursor.execute(sql)
 rows = cursor.fetchall()
 for i in rows:
-	for j in range(3):
+	for j in range(5):
 		print(i[j])
+'''
+while(1):
+	pass
 #
-cli_sd.close()
+#cli_sd.close()#in the thread
+ser_sd.close()
